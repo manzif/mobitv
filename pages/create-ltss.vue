434e476b78f1a348e5f560ac3e39e53706f157b0
Manzi Fabrice <manzif60@gmail.com>
2021-03-09 09:57:59 +0200

    create a rnit

--- pages/create-ltss.vue
+++ pages/create-ltss.vue
@@ -5,39 +5,10 @@
         <v-col cols="12" md="9" sm="6">
           <v-card text class="pa-7">
             <v-col cols="12" md="12" sm="6">
-              <h2 class="pb-1" color="#0087ff">Pay LTSS.</h2>
+              <h2 class="pb-1" color="#0087ff">
+                This page is under construction.
+              </h2>
               <div class="line"></div>
-              <h4 class="my-6">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut,
-                suscipit a. Debitis placeat asperiores corrupti dolores? Vel
-                voluptatibus aut animi, quas doloribus, itaque molestiae ipsum
-                officia at natus mollitia iure?
-              </h4>
-            </v-col>
-            <v-col cols="12" md="12" sm="6">
-              <v-form ref="form" v-model="isFormValid" :lazy-validation="lazy">
-                <v-flex xs12 md12>
-                  <v-text-field
-                    v-model="nid"
-                    :rules="[(v) => !!v || 'National Id Number is required']"
-                    label="National Id"
-                    required
-                    dense
-                    single-line
-                    outlined
-                  ></v-text-field>
-                </v-flex>
-                <v-flex xs12 md12>
-                  <v-btn
-                    :loading="isLoading"
-                    :disabled="!isFormValid"
-                    @click="nidValidation"
-                    color="primary"
-                    block
-                    >Submit</v-btn
-                  >
-                </v-flex>
-              </v-form>
             </v-col>
           </v-card>
         </v-col>
@@ -45,48 +16,6 @@
     </div>
   </v-container>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      nid: '',
-      paymentYear: '',
-      isFormValid: false,
-      typeItems: ['2020', '2021'],
-      lazy: false
-    }
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters['helper/isLoading']
-    },
-    isDisabled() {
-      return this.$store.getters['helper/isDisabled']
-    }
-  },
-  created() {
-    console.log('\n\n\n', this.$route.query)
-  },
-  methods: {
-    async nidValidation() {
-      this.$store.dispatch('helper/loading')
-      this.$store.dispatch('helper/disabling')
-      try {
-        await this.$store.dispatch('ltss/nidValidation', {
-          identification: this.nid
-          //   paymentYear: this.paymentYear,
-          //   clientId: this.$route.query.clid,
-          //   clientPhone: this.$route.query.clientPhone
-        })
-        this.$store.dispatch('helper/loading')
-        this.$store.dispatch('helper/disabling')
-      } catch (e) {
-        return e
-      }
-    }
-  }
-}
-</script>
 <style scoped>
 .line {
   width: 63px;
--- pages/create-rnit.vue
+++ pages/create-rnit.vue
@@ -16,50 +16,6 @@
     </div>
   </v-container>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      title: '',
-      userName: '',
-      isFormValid: false,
-      typeItems: ['2019', '2020', '2021'],
-      description: '',
-      userId: ''
-    }
-  },
-  computed: {
-    isLoading() {
-      return this.$store.getters['helper/isLoading']
-    },
-    isDisabled() {
-      return this.$store.getters['helper/isDisabled']
-    }
-  },
-  methods: {
-    async createApp() {
-      this.$store.dispatch('helper/loading')
-      this.$store.dispatch('helper/disabling')
-      try {
-        await this.$store.dispatch('app/createApp', {
-          title: this.title,
-          userName: this.userName,
-          userId: this.userId,
-          description: this.description
-        })
-        this.$store.dispatch('helper/loading')
-        this.$store.dispatch('helper/disabling')
-        this.title = null
-        this.userId = null
-        this.userName = null
-        this.description = null
-      } catch (e) {
-        return e
-      }
-    }
-  }
-}
-</script>
 <style scoped>
 .line {
   width: 63px;
